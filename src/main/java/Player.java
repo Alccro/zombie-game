2463105cacfcd2365c6054144cf2a226560c7d53
Al Crombie <farnelali@gmail.com>
2022-11-08 21:11:12 +0100

    X moves without using clearscreen. Uses blank character to overwrite previous position.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -9,30 +9,44 @@ public class Player {
   private int lives = 1;
   private int x;
   private int y;
+  private int prevX;
+  private int prevY;
+
 
   public Player(int lives, int x, int y) {
     this.lives = lives;
     this.x = x;
     this.y = y;
+    this.prevX = x;
+    this.prevY = y;
   }
 
   public int getX() {
     return x;
+  }
+  public void setPrevX(int x) { this.prevX = x; }
+  public int getPrevX() {
+    return prevX;
   }
   public void setX(int x) { this.x = x; }
 
   public int getY() {
     return y;
   }
+  public void setPrevY(int y) { this.prevY = y; }
+  public int getPrevY() {
+    return prevY;
+  }
   public void setY(int y) { this.y = y; }
 
   public Character movePlayer(Terminal t) throws InterruptedException, IOException {
     KeyStroke keyStroke = null;
 
-    t.clearScreen();
     t.setCursorPosition(getX(), getY());
     t.putCharacter(player);
     t.flush();
+    setPrevX(getX());
+    setPrevY(getY());
 
     do {
       Thread.sleep(5);
@@ -54,6 +68,10 @@ public class Player {
       case ArrowRight:
         setX(getX() + 1);
     }
+    t.setCursorPosition(getPrevX(), getPrevY());
+    t.putCharacter(' ');
+    t.flush();
+
     return c;
   }
 
